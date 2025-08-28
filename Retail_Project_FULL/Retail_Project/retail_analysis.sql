09b30247ecf40d66b02cd5d43a8b076f92e5ea9d
KOTTIVINAY <kottivinay1992@gmail.com>
2025-08-29 00:29:06 +0530

    Delete Retail_Project_FULL/Retail_Project directory

--- Retail_Project_FULL/Retail_Project/retail_analysis.sql
+++
@@ -1,9 +0,0 @@
-
--- Retail Project SQL Queries
-SELECT strftime('%m-%Y', Date) as Month, SUM(Sales) as Total_Sales
-FROM Retail_Sales
-GROUP BY Month;
-
-SELECT Category, SUM(Profit) as Total_Profit
-FROM Retail_Sales
-GROUP BY Category;
